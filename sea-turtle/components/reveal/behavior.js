0b08409c534a8cd16c2a0e78b81f7ce833f66637
Adam Walzer <adamwalzer@gmail.com>
2016-01-06 17:54:09 -0500

    adding sea turtle

---
+++ sea-turtle/components/reveal/behavior.js
@@ -0,0 +1,34 @@
+pl.game.component('reveal', function () {
+
+	this.item = function (_id) {
+		var vo, index;
+
+		this.closeAll();
+
+		if (typeof _id === 'number') {
+			this.open(this.find('li').eq(_id));
+			this.audio.voiceOver[_id].play();
+
+		}
+			
+		else if (typeof _id === 'string') {
+			if (this[_id]) {
+				this.open(this[_id]);
+
+				if (this.audio) {
+					index = this[_id].index();
+					vo = this.audio.voiceOver[_id] || this.audio.voiceOver[index];
+					
+					if (vo) vo.play();
+				}
+			}
+		}
+	};
+
+	this.closeAll = function() {
+		if(!this.screen.state(this.screen.STATE.VOICE_OVER)) {
+			this.close(this.find('li.OPEN'));
+		}
+	}
+
+});
