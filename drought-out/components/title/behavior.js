a3be93b2efc4a2868ec80800f98b2c350ec341a8
Adam Walzer <adamwalzer@gmail.com>
2016-02-03 12:22:28 -0500

    drought out update for sprites

--- drought-out/components/title/behavior.js
+++ drought-out/components/title/behavior.js
@@ -3,7 +3,7 @@ pl.game.component('title', function () {
 	this.ready = function () {
 		this.open();
 		this.audio.background.play();
-		this.delay(1500, function () {
+		this.delay(3000, function () {
 			this.complete();
 		});
 	};
