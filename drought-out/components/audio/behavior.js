798989aad81d631c0570298ca15019f4f39169f8
Adam Walzer <adamwalzer@gmail.com>
2016-02-05 17:39:39 -0500

    drought out

---
+++ drought-out/components/audio/behavior.js
@@ -0,0 +1,13 @@
+pl.game.component('audio-sequence', function () {
+	
+	// this.sounds = null;
+
+	// this.on('ready', function(_event) {
+	// 	if (!this.is(_event.target)) return;
+	// 	this.sounds = this.find("> audio");
+	// });
+
+	// this.start = function() {
+		
+	// };
+});
