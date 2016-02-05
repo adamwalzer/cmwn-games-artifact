2ac55fe562703fd59dc2d00d207aa0bc4f9b3717
Adam Walzer <adamwalzer@gmail.com>
2016-02-05 11:18:16 -0500

    animal id update

--- animal-id/components/reveal/behavior.js
+++ animal-id/components/reveal/behavior.js
@@ -67,13 +67,13 @@ pl.game.component('reveal', function () {
 	};
 
 	this.closeAll = function() {
-		if(!this.screen.state(this.screen.STATE.VOICE_OVER)) {
+		if(!this.screen.state(this.screen.STATE.VOICE_OVER) || this.game.demoMode) {
 			this.close(this.find('li.OPEN'));
 		}
 	};
 
 	this.handleCloseClick = function() {
-		if(!this.screen.state(this.screen.STATE.VOICE_OVER)) {
+		if(!this.screen.state(this.screen.STATE.VOICE_OVER) || this.game.demoMode) {
 			this.closeAll();
 			this.screen.next();
 		}
