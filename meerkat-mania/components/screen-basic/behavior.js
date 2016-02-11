6f1f18c49f04546b8271cab6317d269dd19f7022
Adam Walzer <adamwalzer@gmail.com>
2016-02-11 15:59:43 -0500

    meerkat to staging

---
+++ meerkat-mania/components/screen-basic/behavior.js
@@ -0,0 +1,115 @@
+pl.game.component('screen-basic', function () {
+	
+	this.currentVO = null;
+
+	this.playSound = function (_sound) {
+		var delay;
+	
+		delay = $(_sound).attr('pl-delay');
+
+		if($(_sound).hasClass('voice-over')) {
+			this.currentVO = _sound;
+		}
+
+		if (delay) {
+			return this.delay(delay, _sound.play.bind(_sound));
+		} else {
+			return _sound.play();
+		}
+	};
+
+	this.next = function () {
+		var nextScreen, buttonSound;
+
+		nextScreen = this.proto();
+		buttonSound = pl.util.resolvePath(this, 'game.audio.sfx.button');
+
+		if (nextScreen) {
+			this.leave();
+			nextScreen.open();
+			if (buttonSound) buttonSound.play();
+		}
+
+		return nextScreen;
+	};
+
+	this.prev = function () {
+		var prevScreen, buttonSound;
+
+		prevScreen = this.proto();
+		buttonSound = pl.util.resolvePath(this, 'game.audio.sfx.button');
+
+		if (prevScreen) {
+			this.screen.close();
+			prevScreen.open();
+			if (buttonSound) buttonSound.play();
+		}
+
+		return prevScreen;
+	};
+
+	this.start = function () {
+		var bgSound, voSound;
+
+		bgSound = pl.util.resolvePath(this, 'audio.background[0]?');
+		voSound = pl.util.resolvePath(this, 'audio.voiceOver[0]?');
+		fxSound = pl.util.resolvePath(this, 'audio.sfx.start');
+
+		if (bgSound) {
+			this.game.bgSound = bgSound;
+			bgSound.play();
+		}
+		if(fxSound) fxSound.play();
+		if (voSound && !voSound.hasAttribute("pl-dontautoplay")) this.playSound(voSound);
+
+		if (this.hasOwnProperty('entities') && this.entities[0]) this.entities[0].start();
+
+		return this;
+	};
+
+	this.stop = function() {
+		if(this.timeoutID) {
+			clearTimeout(this.timeoutID);
+		}
+
+		if(this.currentVO) {
+			this.currentVO.pause();
+			this.currentVO.currentTime = 0;
+		}
+	};
+
+	this.on('ui-open', function (_event) {
+		if (this.isReady && this === _event.targetScope) {
+			this.start();
+		}
+
+		if(this.properties.gameClass) {
+			this.game.addClass(this.properties.gameClass);
+		}
+
+		if (!this.requiredQueue || (this.hasOwnProperty('requiredQueue') && !this.requiredQueue.length)) {
+			this.complete();
+		}
+	});
+
+	this.on('ui-leave', function (_event) {
+		if(this.properties.gameClass) {
+			this.game.removeClass(this.properties.gameClass);
+		}
+
+		if (this.isReady && this === _event.targetScope) {
+			this.stop();
+		}
+	});
+
+	this.on('ui-close', function (_event) {
+		if(this.properties.gameClass) {
+			this.game.removeClass(this.properties.gameClass);
+		}
+
+		if (this.isReady && this === _event.targetScope) {
+			this.stop();
+		}
+	});
+
+});
