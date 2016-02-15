e78ae975513d309a732925b5062ea8a2992a8e4d
Adam Walzer <adamwalzer@gmail.com>
2016-02-15 12:43:20 -0500

    printmaster title screen fix

--- printmaster/components/title/behavior.js
+++ printmaster/components/title/behavior.js
@@ -2,8 +2,7 @@ pl.game.component('title', function () {
 
 	this.ready = function () {
 		this.open();
-		this.audio.background.play();
-		this.delay(1500, function () {
+		this.delay(1750, function () {
 			this.complete();
 		});
 	};
