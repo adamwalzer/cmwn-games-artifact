ddc712e3bf05e455bc6e1dc2d51de2aadd290a51
Adam Walzer <adamwalzer@gmail.com>
2016-02-18 12:13:07 -0500

    litterbug update

--- litter-bug/components/no/behavior.js
+++
@@ -1,8 +0,0 @@
-pl.game.component('no', function () {
-
-	this.next = function () {
-		var buttonSound = pl.util.resolvePath(this, 'game.audio.sfx.no');
-		if (this.screen.next() && buttonSound) buttonSound.play();
-	};
-
-});
