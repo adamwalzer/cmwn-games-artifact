557eb20890fa66f697885ba635f4fd645bab8652
Adam Walzer <adamwalzer@gmail.com>
2016-03-11 16:55:18 -0500

    printmaster to production

---
+++ printmaster/components/audio-sequence/behavior.js
@@ -0,0 +1,24 @@
+pl.game.component('audio-sequence', function () {
+	
+	var sounds = null;
+
+	this.on('ready', function(_event) {
+		var screen = this.screen;
+
+		if (!this.is(_event.target)) return;
+
+		sounds = this.find("> audio");
+		sounds.each(function(i, audio) {
+			audio.onended = function() {
+				if(sounds[i+1] && screen.state(screen.STATE.OPEN)) screen.playSound(sounds[i+1]);
+			};
+		});
+		if(typeof this.properties.loop !== 'undefined') {
+			sounds[sounds.length-1].onended = this.start.bind(this);
+		}
+	});
+
+	this.start = function() {
+		if(sounds[0] && this.screen.state(this.screen.STATE.OPEN)) this.screen.playSound(sounds[0]);
+	};
+});
