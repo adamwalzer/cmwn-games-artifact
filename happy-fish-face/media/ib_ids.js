c9599c404e58a2d438c437af553a1f5b8716af13
Annalise Mariottini <annaliseirene@msn.com>
2016-12-15 21:32:04 -0500

    happy fish face updates

---
+++ happy-fish-face/media/ib_ids.js
@@ -0,0 +1,20 @@
+const ID = {
+    VO: {
+        YOU_FEEL: '02473bae6bd7bb5cd3309dee2042a372.mp3',
+        WHAT_CAN: '0fbd45fa0ec27d6731a085133ed20e5a.mp3',
+        WATER_POLLUTION: 'd69bd50354d7cea357393a7a99fb7cc8.mp3',
+    },
+    SFX: {
+        LEFT_EMOJI: '124e1b43318c82ccbe22531ab8557d2d.mp3',
+        CENTER_EMOJI: '71b529e9f56c136b35802c71d4726c80.mp3',
+        RIGHT_EMOJI: 'a046bfce8dc292b0ccf518c49bcf8997.mp3',
+    },
+    IMG: {
+        YOU_FEEL: 'e45aac2e60532d43b8f59d75d066e99e.png',
+    },
+    SPRITE: {
+        EMOJIS: '0449087e3a9df142be90f2ba702e498b.png',
+    },
+};
+
+export default ID;
